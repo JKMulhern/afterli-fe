c4f6ba72611f7d88361698a04118f8ad2867ea30
Jack Mulhern <you@example.com>
2023-08-07 20:11:34 -0400

    updated html and script

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 // Database of food smells
 var foodSmells = {
-  "Eggs": "Strong, sulfurous smell",
-  "Coffee": "Bitter, acrid smell",
-  "Fish": "Mildly fishy smell",
-  "Cheese": "Rich, cheesy smell",
-  "Garlic": "Pungent, lingering smell",
-  "Broccoli": "Strong, earthy smell",
-  "Bacon": "Slightly smoky smell",
-  "Pineapple": "Faintly sweet smell",
+  "Eggs": "Strong, sulfurous farts",
+  "Coffee": "Bitter, acrid farts",
+  "Fish": "Mildly fishy toots",
+  "Cheese": "Rich, cheesy stinkers",
+  "Garlic": "Pungent, lingering odor",
+  "Broccoli": "Strong, earthy farts",
+  "Bacon": "Slightly smoky but still disgusting",
+  "Pineapple": "Faintly sweet, good for sbd farts"
   // Add more food items here with corresponding after-digestion smells
 };
+
+// Function to populate the dropdown
+function populateDropdown() {
+  var dropdown = document.getElementById("food-dropdown");
+  for (var food in foodSmells) {
+    var option = document.createElement("option");
+    option.value = food;
+    option.text = food;
+    dropdown.add(option);
+  }
+}
+
+// Call the function when the page loads
+populateDropdown();
+
 document.getElementById("add-food-form").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent default form submission
+  event.preventDefault();
   
   var foodName = document.getElementById("food-name").value;
   var foodSmell = document.getElementById("food-smell").value;
@@ -26,29 +41,16 @@ document.getElementById("add-food-form").addEventListener("submit", function(eve
   option.text = foodName;
   dropdown.add(option);
   
-  // Optional: Clear the input fields
+  // Clear the input fields
   document.getElementById("food-name").value = "";
   document.getElementById("food-smell").value = "";
 });
 
-// Function to populate the dropdown with food items
-function populateDropdown() {
-  var select = document.getElementById('food-select');
-  for (var food in foodSmells) {
-    var option = document.createElement('option');
-    option.value = food;
-    option.text = food;
-    select.appendChild(option);
-  }
-}
-
 // Function to predict smell based on selected food
 function predictSmell() {
-  var selectedFood = document.getElementById('food-select').value;
+  var selectedFood = document.getElementById('food-dropdown').value;
   var resultDiv = document.getElementById('result');
   var predictedSmell = foodSmells[selectedFood] || "Food not found in the database";
   resultDiv.innerHTML = "The predicted smell for " + selectedFood + " is: " + predictedSmell;
 }
 
-// Call the populateDropdown function on page load
-window.onload = populateDropdown;
